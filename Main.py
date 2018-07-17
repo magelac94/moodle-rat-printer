c67913d83a91bd8d62088fa9d8dd3214144352e7
magelac94 <magelac94@gmail.com>
2018-07-17 17:34:30 -0300

    agrego archivos
    -

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
-print ("Hola")
+from tkinter import *
+import time
+
+def parpadear():
+	ventana.iconify()
+	time.sleep(3)
+	ventana.deiconify()
+
+ventana = Tk()
+ventana.title("Esta es una ventanita")
+
+boton = Button(ventana,text="Evento",command=parpadear)
+boton.pack()
+
+ventana.mainloop()
