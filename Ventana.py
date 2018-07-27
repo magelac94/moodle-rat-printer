c0d5bd201b86d7384b3e18f5d024b96b7cdc93cb
magelac94 <magelac94@gmail.com>
2018-07-26 22:44:17 -0300

    estructura con datos
    queda estructura de datos con datos, falta las preguntas 2 a N

--- Ventana.py
+++ Ventana.py
@@ -4,11 +4,11 @@ from tkinter import *
 from lxml import etree
 import html
 import re
+import xml.etree.ElementTree as ET
 def strip_tags(value):
 	return re.sub(r'<[^>]*?>','',value)
 
-def abrirXML():
-	 
+def manejoDatos(archivo):
 	gridQuiz = [[0,1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]	# Estructura donde se guardaran las preguntas
 	# gridQuiz[x][] # numero pregunta
 	# gridQuiz[][0]	# Titulo
@@ -20,46 +20,38 @@ def abrirXML():
 	# gridQuiz[][6]	# Respuesta 3 ...
 
 
+	tree = ET.parse(archivo)
+	root = tree.getroot()
+	
+	print ("Cantidad de Preguntas del test ", len(root))	# Elementos hijos de la raiz = cantidad de preguntas
 
-	archivo=fd.askopenfilename()	# Abre ventana para seleccionar archivo, devuelve la ruta del archivo	
+	#quiz=raiz[0]	#Primer elemento
 
-	doc = etree.parse(archivo)	# Se guarda contenido de archivo xml en un arbol de la lib lxml	
+	for cuestion in root.findall('question'):
+		titulo = cuestion.find('name/text').text
+		pregunta = strip_tags(cuestion.find('questiontext/text').text)
+		tipopregunta = cuestion.get('type')
+		#imagen = resolver
+		respuesta1 = strip_tags(cuestion.find("answer/text").text)
 
-	raiz=doc.getroot() 	#Obtener elemento raiz
-
-	print ("Cantidad de Preguntas del test ", len(raiz))	# Elementos hijos de la raiz = cantidad de preguntas
-
-	quiz=raiz[0]	#Primer elemento
-
-	# Titulo opcional
-	gridQuiz[0][0]= 2	# agregar titulo pregunta
-
-	# Texto de la Pregunta
-	preguntas = doc.findall("question")
-	p1 = strip_tags(preguntas[0].find("questiontext/text").text)
-	gridQuiz[0][1]= p1
-	print ("PREGUNTA EN LISTA")
-	print(gridQuiz[0][1])
-
-	# Tipo de pregunta	
-	print ("Tipo de Pregunta")
-	gridQuiz[0][2] = "Soy el tipo de Pregunta"   # agregar tipo de pregunta
+		gridQuiz[0][0]= titulo
+		gridQuiz[0][1]= pregunta
+		gridQuiz[0][2] = tipopregunta
+	#	gridQuiz[0][3] = imagen
+		gridQuiz[0][4] = respuesta1
 
 
-	# Imagen
-	print ("Imagen")
-	gridQuiz[0][3] = "Soy el tipo de Pregunta"   # agrgar imagen si hay
+		print ("Titulo Pregunta: ",gridQuiz[0][0])
+		print( "Texto Pregunta: ",gridQuiz[0][1])
+		print ("Tipo de Pregunta: ", gridQuiz[0][2])
+		print ("Respuesta1: ", gridQuiz[0][4])
 
-
-	# Texto 1era Respuesta
-	print ("Respuesta 1")
-	r1=strip_tags(preguntas[0].find("answer/text").text)
-	gridQuiz[0][4]=r1
-	print (gridQuiz[0][4])
-
-
+def abrirXML():
+ 	archivo=fd.askopenfilename()	# Abre ventana para seleccionar archivo, devuelve la ruta del archivo	
+ 	manejoDatos(archivo)
 	
 
+	
 ventana=Tk()
 ventana.title("TBL Printer")
 ventana.config(bg="#0B0B61")
