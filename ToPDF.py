33593ca7d4c2e21c79ce7b99ab64cdde3c4d04b1
magelac94 <magelac94@gmail.com>
2018-07-30 23:29:42 -0300

    formateo pdf
    formateo pdf

--- ToPDF.py
+++ ToPDF.py
@@ -4,6 +4,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch
+from reportlab.pdfgen import *
 
 # LISTA DE PREGUNTAS
 # Estructura donde se guardaran las preguntas /
@@ -19,11 +20,12 @@ from reportlab.lib.units import inch
  
 def convertir(listaPreguntas):
     nombrePrueba = "PROGFUN-RAT1 Lenguaje funcional básico"
+    descripcion = ""
     nombreArchivo = nombrePrueba +".pdf"
 
     documento = SimpleDocTemplate(nombreArchivo,pagesize=letter,
                         rightMargin=72,leftMargin=72,
-                        topMargin=72,bottomMargin=18)
+                        topMargin=50,bottomMargin=18)
     Prueba=[]
     
     #logo = "python_logo.png"
@@ -43,8 +45,15 @@ def convertir(listaPreguntas):
     # DIA Y HORA
     estiloFecha=getSampleStyleSheet()
     estiloFecha.add(ParagraphStyle(name='Right', alignment=TA_RIGHT))
-    lineaFecha = '<font size=12>%s</font>' % diaHora
+    lineaFecha = '<font size=9>%s</font>' % diaHora
     Prueba.append(Paragraph(lineaFecha, estiloFecha["Normal"]))
+    Prueba.append(Spacer(1, 12))
+
+    #Nombre y CI
+    estiloCI=getSampleStyleSheet()
+    estiloCI.add(ParagraphStyle(name='Center', alignment=TA_CENTER))
+    lineaCI = '<font size=9>Nombre y Apellido _____________________________________       CI ________________________  </font>'
+    Prueba.append(Paragraph(lineaCI, estiloCI["Normal"]))
     Prueba.append(Spacer(1, 12))
  
     # NOMBRE DE LA PRUEBA
@@ -53,6 +62,14 @@ def convertir(listaPreguntas):
     lineaTitulo = '<font size=13>%s</font>' % nombrePrueba
     Prueba.append(Paragraph(lineaTitulo, estiloTitulo["Normal"]))  
     Prueba.append(Spacer(1, 12))
+
+    # Descripcion de la Prueba (Opcional)
+    estiloDescripcion=getSampleStyleSheet()
+    estiloDescripcion.add(ParagraphStyle(name='Center', alignment=TA_CENTER))
+    lineaDescripcion = '<font size=11>%s</font>' % descripcion
+    Prueba.append(Paragraph(lineaDescripcion, estiloDescripcion["Normal"]))  
+    Prueba.append(Spacer(1, 12))
+
 
     # Preguntas y Respuestas
     estiloTituloPregunta=getSampleStyleSheet()
@@ -77,19 +94,22 @@ def convertir(listaPreguntas):
             Prueba.append(Paragraph(lineaNumeroPregunta, estiloNumPregunta["Normal"])) 
 
             #Titulo de la Pregunta - Opcional
-            lineaTituloPregunta = '<font size=12>%s</font>' % pregunta[0]
+            lineaTituloPregunta = '<font size=11>%s</font>' % pregunta[0]
             Prueba.append(Paragraph(lineaTituloPregunta, estiloTituloPregunta["Normal"])) 
+            Prueba.append(Spacer(1, 6))
 
             #Texto de la Pregunta
             lineaPregunta = '<font size=12>%s</font>' % pregunta[1]
             Prueba.append(Paragraph(lineaPregunta, estiloPregunta["Normal"])) 
+            Prueba.append(Spacer(1, 12))
 
             # Respuestas
             p = 5+pregunta[4]
 
             for i in range(5,p):
-                lineaRespuesta = '<font size=12>%s</font>' % pregunta[i]
+                lineaRespuesta = '<font size=11>O %s</font>' % pregunta[i]
                 Prueba.append(Paragraph(lineaRespuesta, estiloRespuesta["Normal"]))
+                Prueba.append(Spacer(1, 6))
             Prueba.append(Spacer(1, 12))
 
             numPreg = numPreg + 1         
