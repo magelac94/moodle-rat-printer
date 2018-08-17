7422b9b94f583a2b800d20b7eb9faa53a1d17ab6
magelac94 <magelac94@gmail.com>
2018-08-16 22:14:53 -0300

    Test with unicode
    Test with unicode

--- prueba.py
+++ prueba.py
@@ -1,10 +1,21 @@
 from xhtml2pdf import pisa             # import python module
 
 # Define your data
-sourceHtml1 = "<html><body><p>To PDF or not to <b>PDF</b></p></body></html>"
-sourceHtml2 = "<html><body><p>To PDF or not to <b>PDF</b></p></body></html>"
-html = "<p><meta http-equiv="Content-Type" content="text/html; charset=utf-8">En el lenguaje Haskell, ¿a qué se le llama una <i>definición</i>?<br></p><p>A una asociación de un nombre (identificador) con un valor de un tipo particular.<br></p><p>A la asociación de un nombre (identificador) con su tipo.<br></p><p>A la declaración de una función.<br></p><p>A la firma de una función.<br></p>"
-
+#html = ""<html><p><meta charset=utf-8"><body><p>To PDF or not to <b>PDF</b></p></body></html>""
+html = """
+    <html>
+        <head>
+            <title>Testing</title>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        </head>
+        <body>
+        <p>En el lenguaje Haskell, ¿a qué se le llama una <i>definición</i>?<br></p>
+        <p>A una asociación de un nombre (identificador) con un valor de un tipo particular.<br></p>
+        <p>A la asociación de un nombre (identificador) con su tipo.<br></p><p>A la declaración de una función.<br></p>
+        <p>A la firma de una función.<br></p>
+        </body>
+    </html>
+    """
 outputFilename = "test.pdf"
 
 # Utility function
@@ -18,6 +29,9 @@ def convertHtmlToPdf(html, outputFilename):
             html,                # the HTML to convert
             dest=resultFile)           # file handle to recieve result
 
+    #pisa.CreatePDF(cont.encode, pdf_out, encoding='utf-8')
+
+
     resultFile.close()                 # close output file
     
     # return True on success and False on errors
@@ -27,3 +41,4 @@ def convertHtmlToPdf(html, outputFilename):
 if __name__ == "__main__":
     pisa.showLogging()
     convertHtmlToPdf(html, outputFilename)
+    pisa.startViewer(outputFilename)
